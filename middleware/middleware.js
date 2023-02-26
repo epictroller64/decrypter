b1d743a0cb62eec7bae225f20a1fc4164c0ff870
martinilbi <martin.ilbi@gmail.com>
2023-02-26 17:38:13 +0200

    fix

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -5,7 +5,7 @@ var get_ip = require('ipware')().get_ip;
 async function authMiddleware(request, response, next) {
     const API_KEY = request.query.apiKey
     const REFERER = request.get("referer") || request.get("Referer")
-    var ip_info = get_ip(req);
+    var ip_info = get_ip(request);
     console.log(ip_info);
     const IP = request.socket.remoteAddress
     const ipInfo = await helper.getIPInfo(IP)
