328c12da184a3892ddd85f3d032dfea09ec2604b
martinilbi <martin.ilbi@gmail.com>
2023-02-26 17:36:40 +0200

    Update

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,9 +1,12 @@
 const helper = require("../helpers");
 const Client = require("../models/client");
+var get_ip = require('ipware')().get_ip;
 
 async function authMiddleware(request, response, next) {
     const API_KEY = request.query.apiKey
     const REFERER = request.get("referer") || request.get("Referer")
+    var ip_info = get_ip(req);
+    console.log(ip_info);
     const IP = request.socket.remoteAddress
     const ipInfo = await helper.getIPInfo(IP)
     console.log(ipInfo.data)
